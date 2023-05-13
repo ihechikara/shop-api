d4fad82c23e82dca10d2f7c6103a8d2ed7249933
Ihechikara <ihechivinabba@gmail.com>
2023-05-13 23:55:49 +0100

    feat: add $regex for advanced search by name and company
    - refactor getAllProducts controller with regex search

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,24 @@
+const Product = require("../models/product")
+
+
 const getAllProducts = async (req, res) =>{
-    res.status(200).send("All products")
+    const { featured, company, name } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === "true" ? true : false
+    }
+    if (company) {
+        queryObject.company = { $regex: company, $options: "i"}
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i"}
+    }
+
+
+    const products = await Product.find(queryObject)    
+
+    res.status(200).json({products, numOfProducts: products.length})
 }
 
 module.exports = {
