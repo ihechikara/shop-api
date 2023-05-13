310845d997dfd00b3edf9292f22dde17b0c7fcb1
Ihechikara <ihechivinabba@gmail.com>
2023-05-14 00:55:33 +0100

    feat: add sort to controller
    - add sort logic to getAllProducts controller
    - sort through query parameters

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/product")
 
 
 const getAllProducts = async (req, res) =>{
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -16,7 +16,15 @@ const getAllProducts = async (req, res) =>{
     }
 
 
-    const products = await Product.find(queryObject)    
+    let result = Product.find(queryObject)
+
+    if(sort){
+        const sortList = sort.split(" ").join(" ")
+        result = result.sort(sortList)
+    } else{
+        result = result.sort("createdAt")
+    }
+    const products = await result
 
     res.status(200).json({products, numOfProducts: products.length})
 }
