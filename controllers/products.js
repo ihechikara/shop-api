61b1587b4e5f5a9773780389c3fc943ef0eda736
Ihechikara <ihechivinabba@gmail.com>
2023-05-14 01:08:53 +0100

    feat: add select logic
    -- add logic to return specific data fields

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/product")
 
 
 const getAllProducts = async (req, res) =>{
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, select } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -23,6 +23,11 @@ const getAllProducts = async (req, res) =>{
         result = result.sort(sortList)
     } else{
         result = result.sort("createdAt")
+    }
+
+    if(select){
+        const selectList = select.split(",").join(" ")
+        result = result.select(selectList)
     }
     const products = await result
 
